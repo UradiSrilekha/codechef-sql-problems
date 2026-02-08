aa702d206fa12e8b128490baaa35e5cf436198f7
UradiSrilekha <srilekhauradi@gmail.com>
2026-02-08 18:12:01 +0530

    Fix SQL query for female flights to Cairo
    Correct the destination spelling and format query.

---
+++ and.sql
@@ -0,0 +1,4 @@
+/* write a query below that returns the rows which meet the following conditions
+- Gender - Female
+- Destination - Cairo */
+select *from Flights where  Destination='Cairoio' and gender='female';
