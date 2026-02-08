700085e1eb3c831b6a3becd216deffdc2a8c6314
UradiSrilekha <srilekhauradi@gmail.com>
2026-02-08 18:12:39 +0530

    Add SQL query for flights involving Mumbai
    Write a query to select flights with Mumbai as origin or destination.

---
+++ OR.sql
@@ -0,0 +1,3 @@
+/*write a query that returns the rows which meet the following conditions
+- Either Origin is Mumbai or the Destination is Mumbai */
+select* from flights where origin='Mumbai' or destination='Mumbai';
